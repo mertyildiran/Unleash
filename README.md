70d32b308dc964016c0470917977ecd3d00a2b51
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-03-18 02:43:33 +0200

    UnleashHelp function added.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Unleash your inner power using Unleash Shell
 
 ### Version
 
-0.0.6
+0.0.7
 
 ### Installation
 
--- unleash.c
+++ unleash.c
@@ -1,4 +1,10 @@
-/* unleash.c */
+/************************************************************************
+	@file				unleash.c
+	@author 			Mehmet Mert Yildiran	mert.yildiran@bil.omu.edu.tr
+	@date				Thursday, March 17, 2016
+	@brief				Main program of Unleash Shell
+	@reservedcommands	help, exit, cd
+************************************************************************/
 #define _PROGRAM_NAME "unleash"
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,6 +12,17 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
+#define MAX_LEN 128
+
+char *reserved_cmds[] = {
+	"cd",
+	"help",
+	"exit"
+};
+
+int UnleashReservedCmdCounter() {
+	return sizeof(reserved_cmds) / sizeof(char *);
+}
 
 const char * GetUsername()
 {
@@ -29,7 +46,45 @@ const char * GetHomeDir()
 	return homedir;
 }
 
-void unleash(void)
+void print_image(FILE *fptr);
+
+void print_image(FILE *fptr)
+{
+	char read_string[MAX_LEN];
+
+	while(fgets(read_string,sizeof(read_string),fptr) != NULL)
+		printf("%s",read_string);
+}
+
+int UnleashHelp()
+{
+	char *filename = "ascii-art.txt";
+	FILE *fptr = NULL;
+
+	if((fptr = fopen(filename,"r")) == NULL)
+	{
+		fprintf(stderr,"ASCII art loading failed!\n");
+	} else {
+		print_image(fptr);
+		fclose(fptr);
+	}
+
+	int i;
+	printf("--------------------------------------------------------------\n");
+	printf("WELCOME TO UNLEASH SHELL\n");
+	printf("Type program names and arguments, and hit enter.\n");
+	printf("The following commands are reserved:\n");
+
+	for (i = 0; i < UnleashReservedCmdCounter(); i++) {
+		printf("  %s\n", reserved_cmds[i]);
+	  }
+
+	printf("Use the man command for information on other programs.\n");
+	printf("--------------------------------------------------------------\n");
+	return 1;
+}
+
+void Unleash(void)
 {
 	const char *HOSTNAME[150];
 	char _hostname[150];
@@ -59,12 +114,16 @@ void unleash(void)
 
 		printf("%s@%s:%s$ ", *USERNAME, *HOSTNAME, outcwd);
 		scanf("%s", command);
+		//printf("%s", command);
 
 		if (strcmp(command, "exit") == 0 || strcmp(command, "quit") == 0) {
 			printf("%s\n", "You're a QUITTER!");
 			return;
 		}
-		
+		else if(strcmp(command, "help") == 0) {
+			UnleashHelp();
+		}
+
 	} while (status);
 }
 
@@ -73,7 +132,7 @@ int main(int argc, char **argv)
 	// Load config files, if any.
 
 	// Run command loop.
-	unleash();
+	Unleash();
 
 	// Perform any shutdown/cleanup.
 	return EXIT_SUCCESS;
